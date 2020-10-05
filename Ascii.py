dd16d9d095ba4d40b8d0c2f45f490ecd791e1a88
Pleasedontforgetmypassword <72300818+Pleasedontforgetmypassword@users.noreply.github.com>
2020-10-05 13:33:58 +1100

    Update Ascii.py

--- Ascii.py
+++ Ascii.py
@@ -1,17 +1,24 @@
 from PIL import Image
 
+#grayscale for 10 characters
 Gray10 = "@%#+=-:. "
+
+#grayscale for 70 characters
 Gray70 = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
 
+#returns the characters by using the value
+#number is to specify 10 or 70 characters, Value is the brightness of the pixel
 def char(number, value):
     if number == 70:
         return Gray70[int(round(value//3.696))]
     else:
         return Gray10[int(round(value//28.34))]
 
+#converts the image to monochrome
 def conversion(img):
     return Image.open(img).convert("L")
 
+#checks if the image can be opened
 def valid(image):
     try:
         Image.open(image)
@@ -19,15 +26,31 @@ def valid(image):
     except:
         return False
 
+#writes the image into ascii
 def write(number, image, file):
+    
+    #define width and height
     width = image.size[0]
     height = image.size[1]
-
+    
+    #opens the file in write mode
     with open(file, "w") as key:
+        
+        #loops over the height of the image
         for i in range(height):
+            
+            #loops over the width of the image
             for j in range(width):
+                
+                #returns the brightness at the j'th, i'th pixel
                 gray = image.getpixel((j, i))
+                
+                #writes the ascii character into Image.txt
                 key.write(char(number, gray))
+                
+            #Newline
             key.write("\n")
+        
+        #closes the file
         key.close()
 
